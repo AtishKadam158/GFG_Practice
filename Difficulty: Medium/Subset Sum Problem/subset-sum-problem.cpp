f6579fc3e8eb199219193b497d78127668d4adfc
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-07-08 14:52:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,17 +1,19 @@
 class Solution {
   public:
+    vector<vector<int>>memo;
     bool subset_sum(vector<int>& arr,int target,int curr,int curr_sum){
         if(curr_sum==target)
             return true;
             
         if(curr>=arr.size() || curr_sum > target)
             return false;
-        
-        return subset_sum(arr,target,curr+1,curr_sum+arr[curr]) || 
-        subset_sum(arr,target,curr+1,curr_sum);
+        if(memo[curr][curr_sum]!=-1) return memo[curr][curr_sum];
+        return memo[curr][curr_sum]=subset_sum(arr,target,curr+1,curr_sum+arr[curr]) 
+        || subset_sum(arr,target,curr+1,curr_sum);
     }
     bool isSubsetSum(vector<int>& arr, int sum) {
-        // code here
+        int n=arr.size();
+        memo.assign(n,vector<int>(sum+1,-1));
         return subset_sum(arr,sum,0,0);
     }
 };
