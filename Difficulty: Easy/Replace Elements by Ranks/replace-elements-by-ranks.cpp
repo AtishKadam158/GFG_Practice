69ea62a1c162a64a35b1c9f6211a47e2212f412c
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-08-07 20:07:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Replace Elements by Ranks/replace-elements-by-ranks.cpp
+++ Difficulty: Easy/Replace Elements by Ranks/replace-elements-by-ranks.cpp
@@ -2,14 +2,15 @@ class Solution {
   public:
     void replaceWithRank(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        vector<pair<int, int>> temp;
-        for (int i = 0; i < n; i++) 
-            temp.push_back({arr[i], i});
+        int n=arr.size();
+        vector<pair<int,int>>temp(n);
+        for(int i=0;i<n;i++){
+            temp[i]= {arr[i],i};
+        }
         
-        sort(temp.begin(), temp.end());
-
-        for (int i = 0; i < n; i++) 
+        sort(temp.begin(),temp.end());
+        
+        for(int i=0;i<n;i++)
             arr[temp[i].second] = i;
         
     }
