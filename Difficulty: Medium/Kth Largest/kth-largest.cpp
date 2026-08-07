f310a496c809c929051fcd3ecebf6b31edce2df0
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-08-07 19:36:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Largest/kth-largest.cpp
+++ Difficulty: Medium/Kth Largest/kth-largest.cpp
@@ -2,11 +2,17 @@ class Solution {
   public:
     int kthLargest(vector<int> &arr, int k) {
         // code here
-        priority_queue<int>pq;
-        for(auto itr: arr) 
-            pq.push(itr);
-        for(int i=0;i<k-1;i++)
-            pq.pop();
+        priority_queue<int,vector<int>, greater<int>>pq;
+        int n=arr.size();
+        for(int i=0;i<k;i++) 
+            pq.push(arr[i]);
+        for(int i=k;i<n;i++){
+            if(pq.top() < arr[i]){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        } 
+        
         return pq.top();
     }
 };
