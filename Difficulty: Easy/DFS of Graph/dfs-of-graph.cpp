d5f2c0487a2007017885031e38d1cccad6b2369d
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-09-04 23:27:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,21 +1,24 @@
 class Solution {
-  private:
-    void dfs_traverse(int node,vector<vector<int>>& adj,vector<int>& visi,vector<int>& res){
-        visi[node]=1;
-        res.push_back(node);
-        for(auto itr:adj[node]){
+  public:
+    vector<int>ans;
+    void traverse(vector<vector<int>>& adj,vector<bool>&visi,int i){
+        visi[i]=true;
+        ans.push_back(i);
+        for(auto itr : adj[i]){
             if(!visi[itr]){
-                dfs_traverse(itr,adj,visi,res);
+                visi[itr]=true;
+                traverse(adj,visi,itr);
             }
         }
     }
-  public:
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        int N=adj.size();
-        vector<int>visi(N,0);
-        vector<int>res;
-        dfs_traverse(0,adj,visi,res);
-        return res;
+        int V=adj.size();
+        vector<bool> visi(V,false);
+        for(int i=0;i<V;i++)
+            if(!visi[i])
+                traverse(adj,visi,i);
+            
+        return ans;
     }
 };
