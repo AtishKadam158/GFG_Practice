ec88903f4b972a594c3857d5780974c8a82cf67b
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-09-05 01:19:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -6,31 +6,33 @@ class Solution {
             adj[itr[1]].push_back(itr[0]);
         }
     }
+    bool dfs(int curr,int parent,vector<vector<int>>& adj,vector<int>&visi){
+        visi[curr]=1;
+        
+        for(auto itr:adj[curr]){
+            if(!visi[itr]){
+                if (dfs(itr, curr,adj, visi))
+                    return true;
+            }else if(itr != parent)
+                return true;
+        }
+        
+        return false;
+    }
     bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
         vector<vector<int>>adj(V);
         convert(edges,adj);
+        
         vector<int>visi(V,0);
+        
         for(int i=0;i<V;i++){
             if(!visi[i]){
-                queue<pair<int,int>>q;
-                q.push({i,-1});
-                visi[i] = 1;
-                while(!q.empty()){
-                    pair<int,int>temp=q.front();
-                    q.pop();
-                    int curr=temp.first;
-                    int parent=temp.second;
-                    for(auto itr:adj[curr]){
-                        if(!visi[itr]){
-                            q.push({itr,curr});
-                            visi[itr]=1;
-                        }else if(itr != parent)
-                            return true;
-                    }
-                }
+                pair<int,int> temp={i,-1};
+                if(dfs(i,-1,adj,visi))
+                    return true;
             }
         }
         return false;
-        
     }
 };
