9a3fabd33f2a050bdb38bd1836f2640173d6ea87
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-09-06 16:58:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,30 +1,32 @@
 class Solution {
   public:
-    vector<int>res;
-    
-    void dfs(int i, vector<vector<int>>& adj,vector<int> &vis){
-        vis[i]=1;
-        for(auto itr : adj[i]){
-            if(!vis[itr])
-                dfs(itr,adj,vis);
+    void convert(vector<vector<int>> &adj,vector<vector<int>> &edges){
+        for(auto itr:edges){
+            adj[itr[0]].push_back(itr[1]);
         }
-        res.push_back(i);
     }
-    
+    void dfs(int i,vector<int>&visi,vector<int>&topo,vector<vector<int>>& adj){
+        visi[i]=1;
+        for(auto itr : adj[i]){
+            if(!visi[itr]){
+                dfs(itr,visi,topo,adj);
+            }
+        }
+        topo.push_back(i);
+    }
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
         vector<vector<int>>adj(V);
+        convert(adj,edges);
         
-        for(auto itr : edges){
-            adj[itr[0]].push_back(itr[1]);
-        }
+        vector<int>visi(V,0);
+        vector<int>topo;
         
-        vector<int> vis(V,0);
         for(int i=0;i<V;i++){
-            if(!vis[i])
-                dfs(i,adj,vis);
+            if(!visi[i])
+                dfs(i,visi,topo,adj);
         }
-        reverse(res.begin(),res.end());
-        return res;
+        reverse(topo.begin(),topo.end());
+        return topo;
     }
 };
