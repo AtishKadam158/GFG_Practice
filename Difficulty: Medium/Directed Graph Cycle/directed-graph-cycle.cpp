58c8100dad2236e20cd59695b6a98eb8e5e01904
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-09-06 16:53:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,36 +1,34 @@
 class Solution {
   public:
     void convert(vector<vector<int>> &adj,vector<vector<int>> &edges){
-        for(auto itr : edges)
+        for(auto itr:edges){
             adj[itr[0]].push_back(itr[1]);
-    }
-    bool dfs(int i,vector<vector<int>> &adj,vector<int>&vis,vector<int>&pathvis){
-        vis[i] = 1;
-        pathvis[i] = 1;
-        for (auto itr : adj[i]) {
-            if (!vis[itr]) {
-                if (dfs(itr, adj, vis, pathvis))
-                    return true;
-            }
-            else if (pathvis[itr]) 
-                return true; 
         }
-        pathvis[i] = 0; 
+    }
+    bool dfs(int i,vector<int>&visi,vector<int>&rec_st,vector<vector<int>>&adj){
+        visi[i]=1;
+        rec_st[i]=1;
+        for(auto itr:adj[i]){
+            if(!visi[itr]){
+                if(dfs(itr,visi,rec_st,adj))
+                    return true;
+            }else if(rec_st[itr])
+                return true;
+        }
+        rec_st[i]=0;
         return false;
     }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>>adj(V);
         convert(adj,edges);
-        
-        vector<int>vis(V,0);
-        vector<int>pathvis(V,0);
-        
+        vector<int>visi(V,0);
+        vector<int>rec_st(V,0);
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathvis))
+            if(!visi[i])
+                if(dfs(i,visi,rec_st,adj))
                     return true;
-            }
+            
         }
         return false;
     }
