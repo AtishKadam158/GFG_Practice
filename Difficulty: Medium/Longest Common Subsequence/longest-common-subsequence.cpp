93d24fe416f7632970a86701dcf3bcb134b41838
Atish Rajabhau Kadam <87365207+AtishKadam158@users.noreply.github.com>
2026-09-13 06:35:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,21 @@
 class Solution {
   public:
+    int n,m;
     vector<vector<int>>memo;
-    int solve(int m,string &s1,int n,string &s2){
-        if(m<0 || n<0)
+    int solve(int i,int j,string &s1, string &s2){
+        if(i==0 || j==0)
             return 0;
+        if(memo[i][j]!=-1) return memo[i][j];
+        if(s1[i-1]==s2[j-1])
+            return memo[i][j] = 1+solve(i-1,j-1,s1,s2);
             
-        if(memo[m][n] != -1)
-            return memo[m][n];
-        if(s1[m]==s2[n])
-            return memo[m][n]=1+solve(m-1,s1,n-1,s2);
-        else 
-            return memo[m][n]=max(solve(m,s1,n-1,s2),solve(m-1,s1,n,s2));
-        
+        return memo[i][j] = max(solve(i-1,j,s1,s2),solve(i,j-1,s1,s2));
     }
     int lcs(string &s1, string &s2) {
-        int m=s1.size()-1;
-        int n=s2.size()-1;
-        memo.assign(m+1,vector<int>(n+1,-1));
-        return solve(m,s1,n,s2);
+        // code here
+        n=s1.size();
+        m=s2.size();
+        memo.assign(n+1,vector<int>(m+1,-1));
+        return solve(n,m,s1,s2);
     }
 };
